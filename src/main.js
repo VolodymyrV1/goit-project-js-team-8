d28999c857fa599c064ea9bf5953afa091a4d1b7
Volodymyr <kotsiubkov@gmail.com>
2025-10-14 18:38:07 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import "./js/navbar";
 import "./js/header";
 import "./js/hero";
 import "./js/books";
-import "./js/feedbacks";
+// import "./js/feedbacks";
 import "./js/events";
 import "./js/article";
 import "./js/location";
 import "./js/footer";
 import "./js/book_modal_window";
-import "./js/contact_modal_window"
+import "./js/contact_modal_window";
+
+
